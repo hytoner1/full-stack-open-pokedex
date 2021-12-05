7e93173999bee13f6222dfa4a6fa9f2d96f7f5ec
Roni Hytonen <roni.hytonen93@gmail.com>
2021-12-05 13:06:40 +0200

    Change expected port number

--- cypress/integration/poke_app.js
+++ cypress/integration/poke_app.js
@@ -1,6 +1,6 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:8080')
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('character names are trademarks of Nintendo.')
   })
